b41be10520ff1ac10250d20838ae0035fbbcec0d
HongruCai <henrytsai5683@outlook.com>
2024-10-19 16:21:50 +0800

    update

--- PUMA/scripts/generate_param.sh
+++ PUMA/scripts/generate_param.sh
@@ -1,5 +1,5 @@
 accelerate launch test_llama.py \
-    --model_path output/param/Llama-2-7b-chat-hf/20241018_2006_param_data768.json_ep10_lr0.0003_bch1/checkpoint-2150 \
+    --model_path output/param/Llama-2-7b-chat-hf/ \
     --base_model meta-llama/Llama-2-7b-chat-hf \
     --data_path data/param_data768.json \
     --float16 \
